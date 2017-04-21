c7a9e67e499bf9c228ff5cf2b680eb72a1b558af
Lei Chen <lei.chen@autodesk.com>
2017-04-21 17:51:31 +0800

    Remove test code

--- thread/ThreadCoordinator.cpp
+++ thread/ThreadCoordinator.cpp
@@ -2,10 +2,5 @@
 
 void ThreadCoordinator::AppendJob(std::function<void()> job)
 {
-	if (m_threadTaskQueue.GetTaskQueueSize() > 32)
-	{
-		m_threadTaskQueue.WaitForFree();
-	}
-
 	m_threadTaskQueue.AddJob(job);
 }
