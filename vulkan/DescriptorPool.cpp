48858b9dde51e9cad1e20afabff2a5de5e79285a
Lei Chen <lei.chen@autodesk.com>
2017-06-22 18:50:53 +0800

    Add descriptor pool

---
+++ vulkan/DescriptorPool.cpp
@@ -0,0 +1,27 @@
+#include "DescriptorPool.h"
+
+bool DescriptorPool::Init(const std::shared_ptr<Device>& pDevice,
+	const VkDescriptorPoolCreateInfo& info)
+{
+	if (!DeviceObjectBase::Init(pDevice))
+		return false;
+
+	m_descriptorPoolSizes.resize(info.poolSizeCount);
+	m_descriptorPoolSizes.assign(info.pPoolSizes, info.pPoolSizes + info.poolSizeCount);
+	
+	m_descriptorPoolInfo = info;
+	m_descriptorPoolInfo.pPoolSizes = m_descriptorPoolSizes.data();
+
+	CHECK_VK_ERROR(vkCreateDescriptorPool(GetDevice()->GetDeviceHandle(), &m_descriptorPoolInfo, nullptr, &m_descriptorPool));
+
+	return true;
+}
+
+std::shared_ptr<DescriptorPool> DescriptorPool::Create(const std::shared_ptr<Device>& pDevice,
+	const VkDescriptorPoolCreateInfo& info)
+{
+	std::shared_ptr<DescriptorPool> pDsPool = std::make_shared<DescriptorPool>();
+	if (pDsPool.get() && pDsPool->Init(pDevice, info))
+		return pDsPool;
+	return nullptr;
+}
---
+++ vulkan/DescriptorPool.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "DeviceObjectBase.h"
+
+class DescriptorPool : public DeviceObjectBase
+{
+public:
+	bool Init(const std::shared_ptr<Device>& pDevice, 
+		const VkDescriptorPoolCreateInfo& info);
+
+public:
+	VkDescriptorPoolCreateInfo GetDescriptorSetLayoutBinding() const { return m_descriptorPoolInfo; }
+	VkDescriptorPool GetDeviceHandle() const { return m_descriptorPool; }
+
+public:
+	static std::shared_ptr<DescriptorPool> Create(const std::shared_ptr<Device>& pDevice,
+		const VkDescriptorPoolCreateInfo& info);
+
+protected:
+	VkDescriptorPoolCreateInfo						m_descriptorPoolInfo;
+	std::vector<VkDescriptorPoolSize>				m_descriptorPoolSizes;
+	VkDescriptorPool								m_descriptorPool;
+};
