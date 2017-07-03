333ad4a2432644ce7571ccc95f823737deda754b
Lei Chen <lei.chen@autodesk.com>
2017-07-03 15:29:20 +0800

    typo

--- vulkan/UniformBuffer.cpp
+++ vulkan/UniformBuffer.cpp
@@ -37,8 +37,8 @@ bool UniformBuffer::Init(const std::shared_ptr<Device>& pDevice, uint32_t numByt
 
 std::shared_ptr<UniformBuffer> UniformBuffer::Create(const std::shared_ptr<Device>& pDevice, uint32_t numBytes)
 {
-	std::shared_ptr<UniformBuffer> pIndexBuffer = std::make_shared<UniformBuffer>();
-	if (pIndexBuffer.get() && pIndexBuffer->Init(pDevice, numBytes))
-		return pIndexBuffer;
+	std::shared_ptr<UniformBuffer> pUniformBuffer = std::make_shared<UniformBuffer>();
+	if (pUniformBuffer.get() && pUniformBuffer->Init(pDevice, numBytes))
+		return pUniformBuffer;
 	return nullptr;
 }
