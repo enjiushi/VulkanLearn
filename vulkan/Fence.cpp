eb3831da696a1c2aa07012817fb0aff16f6393f6
Lei Chen <lei.chen@autodesk.com>
2017-07-14 11:17:59 +0800

    More functionalities in fence

--- vulkan/Fence.cpp
+++ vulkan/Fence.cpp
@@ -1,5 +1,7 @@
 #include "Fence.h"
 
+#define UINT64_MAX       0xffffffffffffffffui64
+
 Fence::~Fence()
 {
 	vkDestroyFence(GetDevice()->GetDeviceHandle(), m_fence, nullptr);
@@ -13,6 +15,7 @@ bool Fence::Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<F
 	VkFenceCreateInfo info = {};
 	info.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
 	CHECK_VK_ERROR(vkCreateFence(GetDevice()->GetDeviceHandle(), &info, nullptr, &m_fence));
+	m_signaled = false;
 
 	return true;
 }
@@ -23,4 +26,20 @@ std::shared_ptr<Fence> Fence::Create(const std::shared_ptr<Device>& pDevice)
 	if (pFence.get() && pFence->Init(pDevice, pFence))
 		return pFence;
 	return nullptr;
+}
+
+void Fence::Wait() const
+{
+	if (m_signaled)
+		return;
+
+	CHECK_VK_ERROR(vkWaitForFences(GetDevice()->GetDeviceHandle(), 1, &m_fence, VK_TRUE, UINT64_MAX));
+}
+
+void Fence::Reset()
+{
+	if (!m_signaled)
+		return;
+
+	CHECK_VK_ERROR(vkResetFences(GetDevice()->GetDeviceHandle(), 1, &m_fence));
 }
--- vulkan/Fence.h
+++ vulkan/Fence.h
@@ -11,10 +11,14 @@ public:
 
 public:
 	VkFence GetDeviceHandle() const { return m_fence; }
+	bool Signaled() const { return m_signaled; }
+	void Reset();
+	void Wait() const;
 
 public:
 	static std::shared_ptr<Fence> Create(const std::shared_ptr<Device>& pDevice);
 
 protected:
 	VkFence	m_fence;
+	bool	m_signaled;
 };
