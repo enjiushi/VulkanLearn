ee68bbeb1c8c5b6572d30e5e82d52299cb36f127
enjiushi <enjiushi@gmail.com>
2018-03-14 21:52:01 +0800

    Add input attachment usage

--- vulkan/Texture2D.cpp
+++ vulkan/Texture2D.cpp
@@ -99,7 +99,7 @@ std::shared_ptr<Texture2D> Texture2D::CreateEmptyTexture(const std::shared_ptr<D
 		pTexture->m_accessFlags = VK_ACCESS_SHADER_READ_BIT;
 	}
 
-	if (pTexture.get() && pTexture->Init(pDevice, pTexture, width, height, format, VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL))
+	if (pTexture.get() && pTexture->Init(pDevice, pTexture, width, height, format, VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL))
 		return pTexture;
 	return nullptr;
 }
@@ -114,7 +114,7 @@ std::shared_ptr<Texture2D> Texture2D::CreateOffscreenTexture(const std::shared_p
 		pTexture->m_accessFlags = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
 	}
 
-	if (pTexture.get() && pTexture->Init(pDevice, pTexture, width, height, format, VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL))
+	if (pTexture.get() && pTexture->Init(pDevice, pTexture, width, height, format, VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL))
 		return pTexture;
 	return nullptr;
 }
