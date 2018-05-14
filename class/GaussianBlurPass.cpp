f7ca2db1d9f270c4a23a7fd4e15062b7d93a036d
enjiushi <enjiushi@gmail.com>
2018-05-14 21:59:44 +0800

    Change ssao pass final layout from color output to shader read only

--- class/GaussianBlurPass.cpp
+++ class/GaussianBlurPass.cpp
@@ -12,7 +12,7 @@ bool GaussianBlurPass::Init(const std::shared_ptr<GaussianBlurPass>& pSelf, VkFo
 	std::vector<VkAttachmentDescription> attachmentDescs(1);
 
 	attachmentDescs[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	attachmentDescs[0].finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+	attachmentDescs[0].finalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 	attachmentDescs[0].format = format;
 	attachmentDescs[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
 	attachmentDescs[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
