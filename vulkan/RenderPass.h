e0e5305c5e1d822c3aa40a8fd6b3880fecf26b04
enjiushi <enjiushi@gmail.com>
2018-10-08 11:13:40 +0800

    Add a new interface to obtain render pass information

--- vulkan/RenderPass.h
+++ vulkan/RenderPass.h
@@ -16,14 +16,6 @@ public:
 protected:
 	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<RenderPass>& pSelf, const VkRenderPassCreateInfo& renderPassInfo);
 
-public:
-	VkRenderPass GetDeviceHandle() const { return m_renderPass; }
-	std::vector<VkAttachmentDescription> GetAttachmentDesc() const { return m_attachmentDescList; }
-	void CacheSecondaryCommandBuffer(const std::shared_ptr<CommandBuffer>& pCmdBuffer);
-	void ExecuteCachedSecondaryCommandBuffers(const std::shared_ptr<CommandBuffer>& pCmdBuffer);
-
-protected:
-
 	typedef struct _SubpassDef
 	{
 		std::vector<VkAttachmentReference>		m_colorAttachmentRefs;
@@ -33,6 +25,14 @@ protected:
 		std::vector<VkAttachmentReference>		m_resolveAttachmentRefs;
 	}SubpassDef;
 
+public:
+	VkRenderPass GetDeviceHandle() const { return m_renderPass; }
+	std::vector<VkAttachmentDescription> GetAttachmentDesc() const { return m_attachmentDescList; }
+	std::vector<SubpassDef> GetSubpassDef() const { return m_subpasses; }
+	void CacheSecondaryCommandBuffer(const std::shared_ptr<CommandBuffer>& pCmdBuffer);
+	void ExecuteCachedSecondaryCommandBuffers(const std::shared_ptr<CommandBuffer>& pCmdBuffer);
+
+protected:
 	bool									m_inited = false;
 	VkRenderPass							m_renderPass;
 	VkRenderPassCreateInfo					m_renderPassInfo;
