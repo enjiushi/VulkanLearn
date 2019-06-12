97638da6bfc07aa498d03a03cbe712d2268de289
enjiushi <enjiushi@gmail.com>
2019-06-12 21:15:46 +0800

    Fix bugs, espacially that "out" and "inout", former one does not care about input and the value is undefined, while later one did faithfully transport input values

--- data/shaders/quaternion.sh
+++ data/shaders/quaternion.sh
@@ -1,19 +1,25 @@
-void Conjugate(out vec4 q)
+void Conjugate(inout vec4 q)
 {
 	q.x *= -1;
 	q.y *= -1;
 	q.z *= -1;
 }
 
-vec4 QuaternionMultiply(vec4 left, vec4 right)
+void Conjugate(inout mat2x4 dq)
 {
-	return vec4(left.w * right.xyz + right.w * left.xyz + cross(left.xyz, right.xyz), left.w * right.w - dot(left.xyz, right.xyz));
+	Conjugate(dq[0]);
+	Conjugate(dq[1]);
 }
 
-http://donw.io/post/dual-quaternion-skinning/
+vec4 QuaternionMultiply(vec4 left, vec4 right)
+{
+    return vec4(left.w * right.xyz + right.w * left.xyz + cross(left.xyz, right.xyz), left.w * right.w - dot(left.xyz, right.xyz));
+}
+
+//http://donw.io/post/dual-quaternion-skinning/
 vec3 QuaternionRotate(vec4 rotation, vec3 p)
 {
-	return p + 2 * cross(rotation.xyz, rotation.w * p + cross(rotation, p));
+	return p + 2 * cross(rotation.xyz, rotation.w * p + cross(rotation.xyz, p));
 }
 
 vec4 AcquireTranslation(mat2x4 dq)
@@ -24,5 +30,13 @@ vec4 AcquireTranslation(mat2x4 dq)
 	// 0.5f * t * r = q
 	// r = p
 	// ===> t = 2 * q * p^-1
-	return 2 * QuaternionMultiply(dq[0] * revert_p);
+	return 2 * QuaternionMultiply(dq[1], revert_p);
+}
+
+vec3 DualQuaternionTransform(mat2x4 dq, vec3 p)
+{
+	vec4 translate = AcquireTranslation(dq);
+	vec3 ret = QuaternionRotate(dq[0], p);
+	ret += translate.xyz;
+	return ret;
 }
