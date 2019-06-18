ec55a5e2123edc2d9ff47043e5dbd8885d3c1bf7
enjiushi <enjiushi@gmail.com>
2019-06-18 14:28:50 +0800

    Create animation data structure and assembly it accordingly

--- class/SkeletonAnimation.cpp
+++ class/SkeletonAnimation.cpp
@@ -5,46 +5,76 @@
 #include <codecvt>
 #include <locale>
 
-bool SkeletonAnimation::Init(const std::shared_ptr<SkeletonAnimation>& pSelf, const aiAnimation* pAssimpAnimation)
+bool SkeletonAnimation::Init(const std::shared_ptr<SkeletonAnimation>& pSelf, const aiScene* pAssimpScene)
 {
 	if (!SelfRefBase<SkeletonAnimation>::Init(pSelf))
 		return false;
 
-	for (uint32_t i = 0; i < pAssimpAnimation->mNumChannels; i++)
+	for (uint32_t i = 0; i < pAssimpScene->mNumAnimations; i++)
 	{
-		std::wstring boneName = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(pAssimpAnimation->mChannels[i]->mNodeName.C_Str());
-		uint32_t chunkIndex = UniformData::GetInstance()->GetPerFrameBoneUniforms()->AllocatePerObjectChunk();
-
-		m_boneDiction[boneName] = chunkIndex;
-
-		UniformData::GetInstance()->GetPerFrameBoneUniforms()->SetAnimationTransform(chunkIndex, AcquireTransformDQ(pAssimpAnimation->mChannels[i], 0));
+		std::wstring animationName = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(pAssimpScene->mAnimations[i]->mName.C_Str());
+		m_animationDataDiction[animationName] = {};
+		AssemblyAnimationData(pAssimpScene->mAnimations[i], m_animationDataDiction[animationName]);
 	}
 
 	return true;
 }
 
-std::shared_ptr<SkeletonAnimation> SkeletonAnimation::Create(const aiAnimation* pAssimpAnimation)
+std::shared_ptr<SkeletonAnimation> SkeletonAnimation::Create(const aiScene* pAssimpScene)
 {
 	std::shared_ptr<SkeletonAnimation> pSkeletonAnimation = std::make_shared<SkeletonAnimation>();
-	if (pSkeletonAnimation != nullptr && pSkeletonAnimation->Init(pSkeletonAnimation, pAssimpAnimation))
+	if (pSkeletonAnimation != nullptr && pSkeletonAnimation->Init(pSkeletonAnimation, pAssimpScene))
 		return pSkeletonAnimation;
 
 	return nullptr;
 }
 
-std::vector<std::shared_ptr<SkeletonAnimation>> SkeletonAnimation::Create(const aiScene* pAssimpScene)
-{
-	std::vector<std::shared_ptr<SkeletonAnimation>> animations;
-	for (uint32_t i = 0; i < pAssimpScene->mNumAnimations; i++)
-		animations.push_back(Create(pAssimpScene->mAnimations[i]));
-
-	return animations;
-}
-
 DualQuaternionf SkeletonAnimation::AcquireTransformDQ(const aiNodeAnim* pAssimpNodeAnim, uint32_t key)
 {
 	Quaternionf rotation = AssimpDataConverter::AcquireQuaternion(pAssimpNodeAnim->mRotationKeys[key].mValue);
-	Vector3f scale = AssimpDataConverter::AcquireVector3(pAssimpNodeAnim->mScalingKeys[key].mValue);
 	Vector3f position = AssimpDataConverter::AcquireVector3(pAssimpNodeAnim->mPositionKeys[key].mValue);
+
 	return {};
+}
+
+void SkeletonAnimation::AssemblyAnimationData(const aiAnimation* pAssimpAnimation, AnimationData& animationData)
+{
+	animationData.animationName = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(pAssimpAnimation->mName.C_Str());
+	animationData.duration = pAssimpAnimation->mDuration / pAssimpAnimation->mTicksPerSecond;
+
+	for (uint32_t i = 0; i < pAssimpAnimation->mNumChannels; i++)
+	{
+		std::wstring objectName = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(pAssimpAnimation->mChannels[i]->mNodeName.C_Str());
+		animationData.objectAnimationDiction[objectName] = {};
+		AssemblyObjectAnimation(pAssimpAnimation->mChannels[i], animationData.objectAnimationDiction[objectName]);
+	}
+}
+
+void SkeletonAnimation::AssemblyObjectAnimation(const aiNodeAnim* pAssimpNodeAnimation, ObjectAnimation& objectAnimation)
+{
+	objectAnimation.objectName = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(pAssimpNodeAnimation->mNodeName.C_Str());
+
+	for (uint32_t i = 0; i < pAssimpNodeAnimation->mNumRotationKeys; i++)
+	{
+		RotationKeyFrame rotationKeyFrame = {};
+		rotationKeyFrame.time = pAssimpNodeAnimation->mRotationKeys[i].mTime;
+		rotationKeyFrame.transform = AssimpDataConverter::AcquireQuaternion(pAssimpNodeAnimation->mRotationKeys[i].mValue);
+		objectAnimation.rotationKeyFrames.push_back(rotationKeyFrame);
+	}
+
+	for (uint32_t i = 0; i < pAssimpNodeAnimation->mNumPositionKeys; i++)
+	{
+		TranslationKeyFrame translationKeyFrame = {};
+		translationKeyFrame.time = pAssimpNodeAnimation->mRotationKeys[i].mTime;
+		translationKeyFrame.transform = AssimpDataConverter::AcquireVector3(pAssimpNodeAnimation->mPositionKeys[i].mValue);
+		objectAnimation.translationKeyFrames.push_back(translationKeyFrame);
+	}
+
+	for (uint32_t i = 0; i < pAssimpNodeAnimation->mNumScalingKeys; i++)
+	{
+		ScaleKeyFrame scaleKeyFrame = {};
+		scaleKeyFrame.time = pAssimpNodeAnimation->mRotationKeys[i].mTime;
+		scaleKeyFrame.transform = AssimpDataConverter::AcquireVector3(pAssimpNodeAnimation->mScalingKeys[i].mValue);
+		objectAnimation.ScaleKeyFrames.push_back(scaleKeyFrame);
+	}
 }
--- class/SkeletonAnimation.h
+++ class/SkeletonAnimation.h
@@ -9,18 +9,53 @@ struct aiScene;
 struct aiAnimation;
 struct aiNodeAnim;
 
+typedef struct _RotationKeyFrame
+{
+	float			time;			// When does this key frame start
+	Quaternionf		transform;		// Rotation
+}RotationKeyFrame;
+
+typedef struct _TranslationKeyFrame
+{
+	float			time;			// When does this key frame start
+	Vector3f		transform;		// Translation
+}TranslationKeyFrame;
+
+typedef struct _ScaleKeyFrame
+{
+	float			time;			// When does this key frame start
+	Vector3f		transform;		// Scale
+}ScaleKeyFrame;
+
+typedef struct _ObjectAnimation
+{
+	std::wstring					objectName;
+	std::vector<RotationKeyFrame>	rotationKeyFrames;		// A list of rotation key frames for this object
+	std::vector<TranslationKeyFrame>translationKeyFrames;	// A list of translation key frames for this object
+	std::vector<ScaleKeyFrame>		ScaleKeyFrames;			// A list of scale key frames for this object
+}ObjectAnimation;
+
+typedef struct _AnimationData
+{
+	std::wstring							animationName;	// Animation name
+	float									duration;		// Total duration of this animation
+	std::map<std::wstring, ObjectAnimation>	objectAnimationDiction;
+}AnimationData;
+
 class SkeletonAnimation : public SelfRefBase<SkeletonAnimation>
 {
 protected:
-	bool Init(const std::shared_ptr<SkeletonAnimation>& pSelf, const aiAnimation* pAssimpAnimation);
+	bool Init(const std::shared_ptr<SkeletonAnimation>& pSelf, const aiScene* pAssimpScene);
 
 public:
-	static std::shared_ptr<SkeletonAnimation> Create(const aiAnimation* pAssimpAnimation);
-	static std::vector<std::shared_ptr<SkeletonAnimation>> Create(const aiScene* pAssimpScene);
+	static std::shared_ptr<SkeletonAnimation> Create(const aiScene* pAssimpScene);
 
 protected:
 	static DualQuaternionf AcquireTransformDQ(const aiNodeAnim* pAssimpNodeAnim, uint32_t key);
+	static void AssemblyAnimationData(const aiAnimation* pAssimpAnimation, AnimationData& animationData);
+	static void AssemblyObjectAnimation(const aiNodeAnim* pAssimpNodeAnimation, ObjectAnimation& objectAnimation);
 
 protected:
-	std::map<std::wstring, uint32_t>	m_boneDiction;
+	std::map<std::wstring, uint32_t>		m_boneDiction;
+	std::map<std::wstring, AnimationData>	m_animationDataDiction;
 };
