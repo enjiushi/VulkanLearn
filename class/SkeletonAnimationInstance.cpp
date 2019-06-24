2deb88cb88b0be34da93c4387b8198cac28dc3c5
enjiushi <enjiushi@gmail.com>
2019-06-24 20:47:09 +0800

    Completely rework SkeletonAnimationInstance initialization accordingly

--- class/SkeletonAnimationInstance.cpp
+++ class/SkeletonAnimationInstance.cpp
@@ -1,37 +1,38 @@
 #include "SkeletonAnimationInstance.h"
 #include "UniformData.h"
+#include "../Maths/AssimpDataConverter.h"
 #include "../common/Macros.h"
+#include "Mesh.h"
 
-bool SkeletonAnimationInstance::Init(const std::shared_ptr<SkeletonAnimationInstance>& pSelf, const std::shared_ptr<SkeletonAnimation>& pSkeletonAnimation)
+bool SkeletonAnimationInstance::Init(const std::shared_ptr<SkeletonAnimationInstance>& pSelf, const std::shared_ptr<SkeletonAnimation>& pSkeletonAnimation, const std::shared_ptr<Mesh>& pMesh)
 {
 	if (!SelfRefBase<SkeletonAnimationInstance>::Init(pSelf))
 		return false;
 
-	//m_perFrameBoneChunkIndexDiction.resize(pSkeletonAnimation->m_animationDataDiction[0].objectAnimationDiction.size());
+	m_pSkeletonAnimation = pSkeletonAnimation;
+	m_pMesh = pMesh;
 
-	//for each (auto animationData in pSkeletonAnimation->m_animationDataDiction)
-	//{
-	//	ASSERTION(m_perFrameBoneChunkIndexDiction.size() == animationData.objectAnimationDiction.size());
+	uint32_t boneCount;
+	ASSERTION(UniformData::GetInstance()->GetPerMeshUniforms()->GetBoneCount(pMesh->GetMeshChunkIndex(), boneCount));
+	m_animationChunk = UniformData::GetInstance()->GetPerAnimationUniforms()->AllocateConsecutiveChunks(boneCount);
 
-	//	for each (auto objectAnimation in animationData.objectAnimationDiction)
-	//	{
-	//		uint32_t chunkIndex = UniformData::GetInstance()->GetPerFrameBoneUniforms()->AllocatePerObjectChunk();
+	uint32_t boneIndex;
+	for each (auto objectAnimation in pSkeletonAnimation->m_animationDataDiction[0].objectAnimationDiction)
+	{
+		ASSERTION(UniformData::GetInstance()->GetPerMeshUniforms()->GetBoneIndex(pMesh->GetMeshChunkIndex(), objectAnimation.objectName, boneIndex));
 
-	//		m_perFrameBoneChunkIndexDiction[UniformData::getbonei()->get]
-	//		perAnimationBoneData.chunkIndexLookupTable.resize()
-	//		
-	//		//UniformData::GetInstance()->GetPerFrameBoneUniforms()->SetAnimationTransform(m_boneAnimationTransformDiction[objectAnimation.first], {});
-	//	}
+		DualQuaternionf dq = { objectAnimation.rotationKeyFrames[0].transform, objectAnimation.translationKeyFrames[0].transform };
 
-	//	m_chunkIndexIndirectBuffer.push_back(perAnimationBoneData);
+		UniformData::GetInstance()->GetPerAnimationUniforms()->SetBoneTransform(m_animationChunk, objectAnimation.objectName, boneIndex, dq);
+	}
 
 	return true;
 }
 
-std::shared_ptr<SkeletonAnimationInstance> SkeletonAnimationInstance::Create(const std::shared_ptr<SkeletonAnimation>& pSkeletonAnimation)
+std::shared_ptr<SkeletonAnimationInstance> SkeletonAnimationInstance::Create(const std::shared_ptr<SkeletonAnimation>& pSkeletonAnimation, const std::shared_ptr<Mesh>& pMesh)
 {
 	std::shared_ptr<SkeletonAnimationInstance> pSkeletonAnimationInstance = std::make_shared<SkeletonAnimationInstance>();
-	if (pSkeletonAnimationInstance != nullptr && pSkeletonAnimationInstance->Init(pSkeletonAnimationInstance, pSkeletonAnimation))
+	if (pSkeletonAnimationInstance != nullptr && pSkeletonAnimationInstance->Init(pSkeletonAnimationInstance, pSkeletonAnimation, pMesh))
 		return pSkeletonAnimationInstance;
 
 	return nullptr;
