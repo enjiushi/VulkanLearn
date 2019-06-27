034e40f36e1030c7ad4ea51a747cdd86287d88c7
enjiushi <enjiushi@gmail.com>
2019-06-27 19:23:32 +0800

    Fill some code to AnimationController, to give it basic functionality to work when it's added to an object

--- component/AnimationController.cpp
+++ component/AnimationController.cpp
@@ -1,11 +1,35 @@
 #include "AnimationController.h"
+#include "../class/SkeletonAnimation.h"
+#include "../class/SkeletonAnimationInstance.h"
+#include "../Base/BaseObject.h"
 
 DEFINITE_CLASS_RTTI(AnimationController, BaseComponent);
 
-bool AnimationController::Init(const std::shared_ptr<AnimationController>& pAnimationController)
+std::shared_ptr<AnimationController> AnimationController::Create(const std::shared_ptr<SkeletonAnimationInstance>& pAnimationInstance)
+{
+	std::shared_ptr<AnimationController> pAnimationController = std::make_shared<AnimationController>();
+	if (pAnimationController.get() && pAnimationController->Init(pAnimationController, pAnimationInstance))
+		return pAnimationController;
+
+	return nullptr;
+}
+
+bool AnimationController::Init(const std::shared_ptr<AnimationController>& pAnimationController, const std::shared_ptr<SkeletonAnimationInstance>& pAnimationInstance)
 {
 	if (!BaseComponent::Init(pAnimationController))
 		return false;
 
+	m_pAnimationInstance = pAnimationInstance;
+
 	return true;
+}
+
+void AnimationController::CallbackFunc(std::shared_ptr<BaseObject>& pObject)
+{
+
+}
+
+void AnimationController::OnAddedToObjectInternal(const std::shared_ptr<BaseObject>& pObject)
+{
+	pObject->RegisterCallbackComponent(GetSelfSharedPtr());
 }
--- component/AnimationController.h
+++ component/AnimationController.h
@@ -2,13 +2,24 @@
 #include "../Base/BaseComponent.h"
 #include "../Maths/Matrix.h"
 
+class SkeletonAnimationInstance;
+
 class AnimationController : public BaseComponent
 {
 	DECLARE_CLASS_RTTI(AnimationController, BaseComponent);
 
 public:
-	static std::shared_ptr<AnimationController> Create();
+	static std::shared_ptr<AnimationController> Create(const std::shared_ptr<SkeletonAnimationInstance>& pAnimationInstance = nullptr);
+
+public:
+	void CallbackFunc(std::shared_ptr<BaseObject>& pObject);
 
 protected:
-	bool Init(const std::shared_ptr<AnimationController>& pAnimationController);
+	bool Init(const std::shared_ptr<AnimationController>& pAnimationController, const std::shared_ptr<SkeletonAnimationInstance>& pAnimationInstance = nullptr);
+
+protected:
+	void OnAddedToObjectInternal(const std::shared_ptr<BaseObject>& pObject) override;
+
+protected:
+	std::shared_ptr<SkeletonAnimationInstance>	m_pAnimationInstance;
 };
