d6822dfd26b104137a628267c290afeba8e57c24
enjiushi <enjiushi@gmail.com>
2019-06-27 20:25:05 +0800

    Calculate bone transform in call back function of animation controller. Do note right now I hard-coded first key frame's transformation and load it into memory for test.

--- component/AnimationController.cpp
+++ component/AnimationController.cpp
@@ -2,6 +2,7 @@
 #include "../class/SkeletonAnimation.h"
 #include "../class/SkeletonAnimationInstance.h"
 #include "../Base/BaseObject.h"
+#include "../Maths/DualQuaternion.h"
 
 DEFINITE_CLASS_RTTI(AnimationController, BaseComponent);
 
@@ -26,7 +27,20 @@ bool AnimationController::Init(const std::shared_ptr<AnimationController>& pAnim
 
 void AnimationController::CallbackFunc(std::shared_ptr<BaseObject>& pObject)
 {
+	std::shared_ptr<SkeletonAnimation> pAnimation = m_pAnimationInstance->GetAnimation();
+	auto iter = pAnimation->m_animationDataDiction[0].objectAnimationLookupTable.find(pObject->GetName());
+	if (iter == pAnimation->m_animationDataDiction[0].objectAnimationLookupTable.end())
+		return;
 
+	Quaternionf& rotation = pAnimation->m_animationDataDiction[0].objectAnimationDiction[iter->second].rotationKeyFrames[0].transform;
+	Vector3f& translate = pAnimation->m_animationDataDiction[0].objectAnimationDiction[iter->second].translationKeyFrames[0].transform;
+
+	pObject->SetRotation(rotation);
+	pObject->SetPos(translate);
+
+	Matrix4f transform = pObject->GetWorldTransform();
+
+	m_pAnimationInstance->SetBoneTransform(pObject->GetName(), DualQuaternionf(transform.RotationMatrix(), transform.TranslationVector()));
 }
 
 void AnimationController::OnAddedToObjectInternal(const std::shared_ptr<BaseObject>& pObject)
