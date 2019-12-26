0462f2a1ca462b4c747253ef7b237a46b7bb982e
enjiushi <enjiushi@gmail.com>
2019-12-26 17:32:18 +0800

    Fix out dated uniform var list in global uniform

--- class/GlobalUniforms.cpp
+++ class/GlobalUniforms.cpp
@@ -595,6 +595,22 @@ std::vector<UniformVarList> GlobalUniforms::PrepareUniformVarList() const
 				},
 				{
 					Vec4Unit,
+					"Main Camera settings0"
+				},
+				{
+					Vec4Unit,
+					"Main Camera settings1"
+				},
+				{
+					Vec4Unit,
+					"Main Camera settings2"
+				},
+				{
+					Vec4Unit,
+					"Main Camera settings3"
+				},
+				{
+					Vec4Unit,
 					"Settings: Gamma, Exposure, White Scale"
 				},
 				{
@@ -615,15 +631,27 @@ std::vector<UniformVarList> GlobalUniforms::PrepareUniformVarList() const
 				},
 				{
 					Vec4Unit,
-					"Temporal Settings1"
-				},
-				{
-					Vec4Unit,
 					"Bloom Settings0"
 				},
 				{
 					Vec4Unit,
 					"Bloom Settings1"
+				},
+				{
+					Vec4Unit,
+					"Depth of field Settings0"
+				},
+				{
+					Vec4Unit,
+					"Motion blur settings"
+				},
+				{
+					Vec4Unit,
+					"Vignette settings"
+				},
+				{
+					Vec4Unit,
+					"SSAO settings"
 				},
 				{
 					Vec4Unit,
