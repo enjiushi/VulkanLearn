f516d8c44c2d708e3248d461760cde35205ad892
enjiushi <enjiushi@gmail.com>
2020-01-03 21:01:18 +0800

    oops

--- class/PerPlanetUniforms.cpp
+++ class/PerPlanetUniforms.cpp
@@ -17,8 +17,8 @@ bool PerPlanetUniforms::Init(const std::shared_ptr<PerPlanetUniforms>& pSelf)
 std::shared_ptr<PerPlanetUniforms> PerPlanetUniforms::Create()
 {
 	std::shared_ptr<PerPlanetUniforms> pPerPlanetUniforms = std::make_shared<PerPlanetUniforms>();
-	if (pPerObjectUniforms.get() && pPerObjectUniforms->Init(pPerObjectUniforms))
-		return pPerObjectUniforms;
+	if (pPerPlanetUniforms.get() && pPerPlanetUniforms->Init(pPerPlanetUniforms))
+		return pPerPlanetUniforms;
 	return nullptr;
 }
 
