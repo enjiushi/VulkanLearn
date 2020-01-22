b240d7e459be9ad1f388a5e952897e310ce76370
enjiushi <enjiushi@gmail.com>
2020-01-22 16:48:59 +0800

    Create saparate queues in global device objects. Though I think I should change the name of graphic queue to general queue. Maybe later

--- vulkan/GlobalDeviceObjects.cpp
+++ vulkan/GlobalDeviceObjects.cpp
@@ -23,7 +23,9 @@ bool GlobalDeviceObjects::InitObjects(const std::shared_ptr<Device>& pDevice)
 	m_pTransferQueue = Queue::Create(pDevice, pDevice->GetPhysicalDevice()->GetTransferQueueIndex());
 	m_pPresentQueue = Queue::Create(pDevice, pDevice->GetPhysicalDevice()->GetPresentQueueIndex());
 
-	m_pMainThreadCmdPool = CommandPool::Create(pDevice);
+	m_pMainThreadGraphicCmdPool = CommandPool::Create(pDevice, m_pDevice->GetPhysicalDevice()->GetGraphicQueueIndex());
+	m_pMainThreadComputeCmdPool = CommandPool::Create(pDevice, m_pDevice->GetPhysicalDevice()->GetComputeQueueIndex());
+	m_pMainThreadTransferCmdPool = CommandPool::Create(pDevice, m_pDevice->GetPhysicalDevice()->GetTransferQueueIndex());
 
 	if (m_pDeviceMemMgr == nullptr)
 		m_pDeviceMemMgr = DeviceMemoryManager::Create(pDevice);
@@ -101,7 +103,9 @@ std::shared_ptr<Queue> GlobalGraphicQueue() { return GlobalObjects()->GetGraphic
 std::shared_ptr<Queue> GlobalComputeQueue() { return GlobalObjects()->GetComputeQueue(); }
 std::shared_ptr<Queue> GlobalTransferQueue() { return GlobalObjects()->GetTransferQueue(); }
 std::shared_ptr<Queue> GlobalPresentQueue() { return GlobalObjects()->GetPresentQueue(); }
-std::shared_ptr<CommandPool> MainThreadPool() { return GlobalObjects()->GetMainThreadCmdPool(); }
+std::shared_ptr<CommandPool> MainThreadGraphicPool() { return GlobalObjects()->GetMainThreadGraphicCmdPool(); }
+std::shared_ptr<CommandPool> MainThreadComputePool() { return GlobalObjects()->GetMainThreadComputeCmdPool(); }
+std::shared_ptr<CommandPool> MainThreadTransferPool() { return GlobalObjects()->GetMainThreadTransferCmdPool(); }
 std::shared_ptr<DeviceMemoryManager> DeviceMemMgr() { return GlobalObjects()->GetDeviceMemMgr(); }
 std::shared_ptr<StagingBufferManager> StagingBufferMgr() { return GlobalObjects()->GetStagingBufferMgr(); }
 std::shared_ptr<SwapChain> GetSwapChain() { return GlobalObjects()->GetSwapChain(); }
--- vulkan/GlobalDeviceObjects.h
+++ vulkan/GlobalDeviceObjects.h
@@ -26,7 +26,9 @@ std::shared_ptr<Queue> GlobalGraphicQueue();
 std::shared_ptr<Queue> GlobalComputeQueue();
 std::shared_ptr<Queue> GlobalTransferQueue();
 std::shared_ptr<Queue> GlobalPresentQueue();
-std::shared_ptr<CommandPool> MainThreadPool();
+std::shared_ptr<CommandPool> MainThreadGraphicPool();
+std::shared_ptr<CommandPool> MainThreadComputePool();
+std::shared_ptr<CommandPool> MainThreadTransferPool();
 std::shared_ptr<DeviceMemoryManager> DeviceMemMgr();
 std::shared_ptr<StagingBufferManager> StagingBufferMgr();
 std::shared_ptr<SwapChain> GetSwapChain();
@@ -56,7 +58,9 @@ public:
 	const std::shared_ptr<Queue> GetComputeQueue() const { return m_pComputeQueue; }
 	const std::shared_ptr<Queue> GetTransferQueue() const { return m_pTransferQueue; }
 	const std::shared_ptr<Queue> GetPresentQueue() const { return m_pPresentQueue; }
-	const std::shared_ptr<CommandPool> GetMainThreadCmdPool() const { return m_pMainThreadCmdPool; }
+	const std::shared_ptr<CommandPool> GetMainThreadGraphicCmdPool() const { return m_pMainThreadGraphicCmdPool; }
+	const std::shared_ptr<CommandPool> GetMainThreadComputeCmdPool() const { return m_pMainThreadComputeCmdPool; }
+	const std::shared_ptr<CommandPool> GetMainThreadTransferCmdPool() const { return m_pMainThreadTransferCmdPool; }
 	const std::shared_ptr<DeviceMemoryManager> GetDeviceMemMgr() const { return m_pDeviceMemMgr; }
 	const std::shared_ptr<StagingBufferManager> GetStagingBufferMgr() const { return m_pStaingBufferMgr; }
 	const std::shared_ptr<SwapChain> GetSwapChain() const { return m_pSwapChain; }
@@ -79,7 +83,9 @@ protected:
 	std::shared_ptr<Queue>					m_pComputeQueue;
 	std::shared_ptr<Queue>					m_pTransferQueue;
 	std::shared_ptr<Queue>					m_pPresentQueue;
-	std::shared_ptr<CommandPool>			m_pMainThreadCmdPool;
+	std::shared_ptr<CommandPool>			m_pMainThreadGraphicCmdPool;
+	std::shared_ptr<CommandPool>			m_pMainThreadComputeCmdPool;
+	std::shared_ptr<CommandPool>			m_pMainThreadTransferCmdPool;
 	std::shared_ptr<DeviceMemoryManager>	m_pDeviceMemMgr;
 
 	std::shared_ptr<StagingBufferManager>	m_pStaingBufferMgr;
