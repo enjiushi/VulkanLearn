baa58f46915db29d3489d6e1029b62221da147a5
enjiushi <enjiushi@gmail.com>
2020-01-22 16:43:37 +0800

    We could define a default image layout when creating an texture 2d array

--- vulkan/Texture2DArray.h
+++ vulkan/Texture2DArray.h
@@ -5,14 +5,15 @@
 class Texture2DArray : public Image
 {
 protected:
-	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<Texture2DArray>& pSelf, const GliImageWrapper& gliTextureArray, VkFormat format);
-	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<Texture2DArray>& pSelf, uint32_t width, uint32_t height, uint32_t mipLevels, uint32_t layers, VkFormat format);
-	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<Texture2DArray>& pSelf, uint32_t width, uint32_t height, uint32_t mipLevels, uint32_t layers, VkFormat format, VkImageUsageFlags usage);
+	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<Texture2DArray>& pSelf, const GliImageWrapper& gliTextureArray, VkFormat format, VkImageLayout defaultLayout);
+	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<Texture2DArray>& pSelf, uint32_t width, uint32_t height, uint32_t mipLevels, uint32_t layers, VkFormat format, VkImageLayout defaultLayout);
+	bool Init(const std::shared_ptr<Device>& pDevice, const std::shared_ptr<Texture2DArray>& pSelf, uint32_t width, uint32_t height, uint32_t mipLevels, uint32_t layers, VkFormat format, VkImageUsageFlags usage, VkImageLayout defaultLayout);
 
 public:
 	static std::shared_ptr<Texture2DArray> Create(const std::shared_ptr<Device>& pDevice, std::string path, VkFormat format);
 	static std::shared_ptr<Texture2DArray> Create(const std::shared_ptr<Device>& pDevice, const GliImageWrapper& gliTextureArray, VkFormat format);
 	static std::shared_ptr<Texture2DArray> CreateEmptyTexture2DArray(const std::shared_ptr<Device>& pDevice, uint32_t width, uint32_t height, uint32_t layers, VkFormat format);
+	static std::shared_ptr<Texture2DArray> CreateEmptyTexture2DArray(const std::shared_ptr<Device>& pDevice, uint32_t width, uint32_t height, uint32_t layers, VkFormat format, VkImageLayout defaultLayout);
 	static std::shared_ptr<Texture2DArray> CreateEmptyTexture2DArray(const std::shared_ptr<Device>& pDevice, uint32_t width, uint32_t height, uint32_t mipLevels, uint32_t layers, VkFormat format);
 	static std::shared_ptr<Texture2DArray> CreateMipmapOffscreenTexture(const std::shared_ptr<Device>& pDevice, uint32_t width, uint32_t height, uint32_t layers, VkFormat format);
 
